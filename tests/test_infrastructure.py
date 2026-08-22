12b91da339e43b716ccfd624b63b86102cbc1466
Guilherme Vital <guivitalrodrigues@gmail.com>
2026-08-21 22:13:03 -0300

    fix: permit Spark directory marker
    Allow only the exact S3 directory marker required by Spark overwrite semantics.

--- tests/test_infrastructure.py
+++ tests/test_infrastructure.py
@@ -15,6 +15,7 @@ class InfrastructureSafetyTests(unittest.TestCase):
         self.assertIn("BytesScannedCutoffPerQuery: 10000000", self.template)
         self.assertIn("MaxConcurrentRuns: 1", self.template)
         self.assertIn("- s3:ListBucket\n", self.template)
+        self.assertIn('curated_$folder$"', self.template)
 
     def test_analytics_layer_is_managed_as_code(self):
         self.assertIn("Type: AWS::Glue::Job", self.template)
